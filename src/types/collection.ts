7f7be50b5d77892000a33a47f4d62258c4c2a739
Kayode Ogunmakinwa <khizzyjr@gmail.com>
2024-02-12 22:45:22 -0500

    chore: added collection types

--- src/types/collection.ts
+++ src/types/collection.ts
@@ -1,1 +1,37 @@
-export type Collection = {}
+import type { UserProfile } from "./user";
+
+export type CollectionRole = 'admin' | 'moderator' | 'viewer';
+export type Collection = {
+    id: string;
+    info: {
+        name: string;
+        slug: string;
+        status: string;
+        description?: string;
+    },
+    assets: {
+        icon: string;
+        thumbnail: string;
+    },
+    links: {
+        url: string;
+        inviteLink?: string;
+    },
+    config: {
+        isPrivate: boolean;
+        allowPublicJoin: boolean;
+    };
+    meta: {
+        views: number;
+    }
+    owner: string;
+    parent: string;
+    updatedBy: string;
+    createdAt: Date | number;
+    updatedAt: Date | number;
+    bookmarkCount: number;
+    isLiked: boolean;
+    role: CollectionRole;
+
+    ownerProfile?: UserProfile;
+}
