9f90efa54cf100e746e00303f7613ffb2081d7f6
Kayode Ogunmakinwa <khizzyjr@gmail.com>
2024-02-21 23:31:21 -0500

    chore: preparing to use the new highlights system

--- src/types/highlight.ts
+++ src/types/highlight.ts
@@ -1,37 +1,66 @@
-import type {OBJECT_ID} from './generic';
+import type { OBJECT_ID } from './generic';
 
 export type Highlight = {
-	/**
-	 * Mongo DB Object ID
-	 */
-	_id: OBJECT_ID;
+    /**
+     * Mongo DB Object ID
+     */
+    _id: OBJECT_ID;
 
-	/**
-	 * UUID V4
-	 */
-	id: string;
+    /**
+     * UUID V4
+     * 
+     * Recogito-js uses this as the ID for the highlight.
+     */
+    id: string;
 
-	type: string;
-	note?: string;
-	owner: OBJECT_ID;
-	bookmark: OBJECT_ID;
-	body: HighlightBody[];
-	target: HighlightTarget;
+    /**
+     * From Recogito-js
+     * 
+     * But we only use "Annotation".
+     */
+    type: "Annotation";
+
+    /**
+     * A note for the highlight.
+     * 
+     * This is not the exact text that was highlighted.
+     */
+    note: string;
+
+    /**
+     * The user who created the highlight.
+     */
+    owner: OBJECT_ID;
+
+    /**
+     * The document that the highlight is in.
+     */
+    bookmark: OBJECT_ID;
+
+    /**
+     * Metadata about the highlight.
+     */
+    body: HighlightBody[];
+
+    /**
+     * Where the highlight is in the document.
+     */
+    target: HighlightTarget;
 };
 
 type HighlightBody = {
-	type: string;
-	value: string;
-	purpose: string;
+    type: string;
+    value: string;
+    purpose: string;
 };
 
 type HighlightTarget = {
-	selector: HighlightTargetSelector[];
+    selector: HighlightTargetSelector[];
 };
 
 type HighlightTargetSelector = {
-	type: string;
-	exact?: string;
-	start?: number;
-	end?: number;
+    type: string;
+    exact?: string;
+    start?: number;
+    end?: number;
 };
